1c13abf276f6a1efa344f9f444887382c7406854
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2024-11-04 19:25:48 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.i2.v1.mbsa.znzn',
+  appId: 'com.i2.v1.mbsa.v4.znzn',
   appName: "Zene 'N Zeanne Au-Some Blogs: The \"Au-Some\" Ice Breaker",
   webDir: 'public',
   bundledWebRuntime: false
